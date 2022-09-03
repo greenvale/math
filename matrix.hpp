7883541cb71d9e01ae344669137e4b210bc45cea
William Denny <wdenny1999@gmail.com>
2022-09-03 22:06:46 +0100

    Baseline complete, adding matrix functions

--- matrix.hpp
+++ matrix.hpp
@@ -8,7 +8,7 @@ William Denny, 3rd September 2022
 #include <assert.h>
 
 template <class T>
-class Matrix2
+class Matrix
 {
 private:
     T* m_data;
@@ -17,48 +17,72 @@ private:
 private:
     int index(int row, int col) const;
 public:
-    // ctors
-    Matrix2();
-    Matrix2(int numRows, int numCols);
-    Matrix2(int numRows, int numCols, const T& value);
-    Matrix2(int numRows, int numCols, const T* data);
-    Matrix2(const Matrix2<T>& matrix); // copy ctor
+    Matrix();
+    Matrix(int numRows, int numCols);
+    Matrix(int numRows, int numCols, const T& value);
+    Matrix(int numRows, int numCols, const T* data);
+    Matrix(const Matrix<T>& matrix); // copy ctor
+    ~Matrix();
     
-    // dtor
-    ~Matrix2();
-    
+    // getters/setters
     T get(int row, int col) const;
+    Matrix<T> getRegion(int row0, int row1, int col0, int col1);
+    Matrix<T> getRow(int row);
+    Matrix<T> getCol(int col);
+    
     void set(int row, int col, const T& value);
+    void setRegion(int row0, int row1, int col0, int col1, const Matrix<T>& matrix);
+    void setRow(int row, const Matrix<T>& matrix);
+    void setCol(int col, const Matrix<T>& matrix);
+    
     int numRows() const;
     int numCols() const;
     
-    void resize(int numRows, int numCols);
-    
+    // useful functions
     void print() const;
     
-    // operator overloading
-    bool operator== (const Matrix2<T>& rhs);
+    // operator overloading (inside class)
+    bool operator== (const Matrix<T>& rhs) const;
     
-    Matrix2<T>& operator= (const Matrix2<T>& rhs);
-    void operator+= (const Matrix2<T>& rhs);
-    void operator-= (const Matrix2<T>& rhs);
+    Matrix<T>& operator= (const Matrix<T>& rhs);
+    void operator+= (const Matrix<T>& rhs);
+    void operator-= (const Matrix<T>& rhs);
     void operator*= (const T& rhs);
     void operator/= (const T& rhs);
+    
+    // matrix functions
+    void resize(int numRows, int numCols);
+    
+    // row/col operations
+    void addRow(int row, const Matrix<T>& matrix);
+    void addCol(int col, const Matrix<T>& matrix);
+    void subtractRow(int row, const Matrix<T>& matrix);
+    void subtractCol(int col, const Matrix<T>& matrix);
+    void scaleRow(int row, const T& coef);
+    void scaleCol(int col, const T& coef);
+    
+    // row reduction (scale row already declared)
+    void replaceRow(int row0, int row1, const T& coef);
+    void swapRow(int row0, int row1);
+    
+    Matrix<T> echelonForm();
+    
 };
 
-template <class U> Matrix2<U> operator+ (const Matrix2<U>& lhs, const Matrix2<U>& rhs);
-template <class U> Matrix2<U> operator+ (const U& lhs, const Matrix2<U>& rhs);
-template <class U> Matrix2<U> operator+ (const Matrix2<U>& lhs, const U& rhs);
+// operator overloading (outside class)
+template <class U> Matrix<U> operator+ (const Matrix<U>& lhs, const Matrix<U>& rhs);
+template <class U> Matrix<U> operator+ (const U& lhs, const Matrix<U>& rhs);
+template <class U> Matrix<U> operator+ (const Matrix<U>& lhs, const U& rhs);
 
-template <class U> Matrix2<U> operator- (const Matrix2<U>& lhs, const Matrix2<U>& rhs);
-template <class U> Matrix2<U> operator- (const U& lhs, const Matrix2<U>& rhs);
-template <class U> Matrix2<U> operator- (const Matrix2<U>& lhs, const U& rhs);
+template <class U> Matrix<U> operator- (const Matrix<U>& lhs, const Matrix<U>& rhs);
+template <class U> Matrix<U> operator- (const U& lhs, const Matrix<U>& rhs);
+template <class U> Matrix<U> operator- (const Matrix<U>& lhs, const U& rhs);
 
-template <class U> Matrix2<U> operator* (const Matrix2<U>& lhs, const Matrix2<U>& rhs);
-template <class U> Matrix2<U> operator* (const U& lhs, const Matrix2<U>& rhs);
-template <class U> Matrix2<U> operator* (const Matrix2<U>& lhs, const U& rhs);
+template <class U> Matrix<U> operator* (const Matrix<U>& lhs, const Matrix<U>& rhs);
+template <class U> Matrix<U> operator* (const U& lhs, const Matrix<U>& rhs);
+template <class U> Matrix<U> operator* (const Matrix<U>& lhs, const U& rhs);
 
-template <class U> Matrix2<U> operator/ (const Matrix2<U>& lhs, const U& rhs);
+template <class U> Matrix<U> operator/ (const Matrix<U>& lhs, const U& rhs);
 
 /* 
 ======================================================================
@@ -66,7 +90,7 @@ CONSTRUCTOR / DESTRUCTOR
 */ 
 
 template <class T>
-Matrix2<T>::Matrix2()
+Matrix<T>::Matrix()
 {
     m_numRows = 0;
     m_numCols = 0;
@@ -74,7 +98,7 @@ Matrix2<T>::Matrix2()
 }
 
 template <class T>
-Matrix2<T>::Matrix2(int numRows, int numCols)
+Matrix<T>::Matrix(int numRows, int numCols)
 {
     m_numRows = numRows;
     m_numCols = numCols;
@@ -82,7 +106,7 @@ Matrix2<T>::Matrix2(int numRows, int numCols)
 }
 
 template <class T>
-Matrix2<T>::Matrix2(int numRows, int numCols, const T&  value)
+Matrix<T>::Matrix(int numRows, int numCols, const T&  value)
 {
     m_numRows = numRows;
     m_numCols = numCols;
@@ -94,7 +118,7 @@ Matrix2<T>::Matrix2(int numRows, int numCols, const T&  value)
 }
 
 template <class T>
-Matrix2<T>::Matrix2(int numRows, int numCols, const T* data)
+Matrix<T>::Matrix(int numRows, int numCols, const T* data)
 {
     m_numRows = numRows;
     m_numCols = numCols;
@@ -106,7 +130,7 @@ Matrix2<T>::Matrix2(int numRows, int numCols, const T* data)
 }
 
 template <class T>
-Matrix2<T>::Matrix2(const Matrix2<T>& matrix)
+Matrix<T>::Matrix(const Matrix<T>& matrix)
 {
     //std::cout << "Copy constructor" << std::endl;
     m_numRows = matrix.numRows();
@@ -122,7 +146,7 @@ Matrix2<T>::Matrix2(const Matrix2<T>& matrix)
 }
 
 template <class T>
-Matrix2<T>::~Matrix2()
+Matrix<T>::~Matrix()
 {
     //std::cout << "Destructor" << std::endl;
     if (m_data != nullptr) 
@@ -138,7 +162,7 @@ GETTER / SETTER
 
 // get element
 template <class T>
-T Matrix2<T>::get(int row, int col) const
+T Matrix<T>::get(int row, int col) const
 {
     assert(row <= m_numRows);
     assert(col <= m_numCols);
@@ -146,9 +170,50 @@ T Matrix2<T>::get(int row, int col) const
     return m_data[index(row, col)];
 }
 
+// get region
+template <class T>
+Matrix<T> Matrix<T>::getRegion(int row0, int row1, int col0, int col1)
+{
+    assert((row0 >= 0) && (row0 < m_numRows));
+    assert((row1 >= 0) && (row1 < m_numRows));
+    assert((row1 >= row0));
+    assert((col0 >= 0) && (col0 < m_numCols));
+    assert((col1 >= 0) && (col1 < m_numCols));
+    assert((col1 >= col0));
+    
+    int numRows = row1 - row0 + 1;
+    int numCols = col1 - col0 + 1;
+    Matrix<T> result(numRows, numCols);
+    
+    for (int i = 0; i < numRows; ++i)
+    {
+        for (int j = 0; j < numCols; ++j)
+        {
+            result.set(i, j, get(i + row0, j + col0));
+        }
+    }
+    
+    return result;
+}
+
+// get row
+template <class T>
+Matrix<T> Matrix<T>::getRow(int row)
+{
+    return getRegion(row, row, 0, m_numCols - 1);
+}
+
+// get column
+template <class T>
+Matrix<T> Matrix<T>::getCol(int col)
+{
+    return getRegion(0, m_numRows - 1, col, col);
+}
+
+
 // set element
 template <class T>
-void Matrix2<T>::set(int row, int col, const T& value)
+void Matrix<T>::set(int row, int col, const T& value)
 {
     assert(row <= m_numRows);
     assert(col <= m_numCols);
@@ -156,28 +221,58 @@ void Matrix2<T>::set(int row, int col, const T& value)
     m_data[index(row, col)] = value;
 }
 
+// set region
+template <class T>
+void Matrix<T>::setRegion(int row0, int row1, int col0, int col1, const Matrix<T>& matrix)
+{
+    assert((row0 >= 0) && (row0 < m_numRows));
+    assert((row1 >= 0) && (row1 < m_numRows));
+    assert((row1 >= row0));
+    assert((col0 >= 0) && (col0 < m_numCols));
+    assert((col1 >= 0) && (col1 < m_numCols));
+    assert((col1 >= col0));
+    
+    int numRows = row1 - row0 + 1;
+    int numCols = col1 - col0 + 1;
+    
+    assert(numRows == matrix.numRows());
+    assert(numCols == matrix.numCols());
+    
+    for (int i = 0; i < numRows; ++i)
+    {
+        for (int j = 0; j < numCols; ++j)
+        {
+            set(i + row0, j + col0, matrix.get(i, j));
+        }
+    }
+}
+
+// set row
+template <class T>
+void Matrix<T>::setRow(int row, const Matrix<T>& matrix)
+{
+    setRegion(row, row, 0, m_numCols - 1, matrix);
+}
+
+// set column
+template <class T>
+void Matrix<T>::setCol(int col, const Matrix<T>& matrix)
+{
+    setRegion(0, m_numRows - 1, col, col, matrix);
+}
+
 // get num rows
 template <class T>
-int Matrix2<T>::numRows() const
+int Matrix<T>::numRows() const
 {
     return m_numRows;
 }
 
 // get num cols
 template <class T>
-int Matrix2<T>::numCols() const
+int Matrix<T>::numCols() const
 {
     return m_numCols;
-}
-
-// resize matrix to new shape
-template <class T>
-void Matrix2<T>::resize(int numRows, int numCols)
-{
-    assert(numRows * numCols == m_numRows * m_numCols);
-    
-    m_numRows = numRows;
-    m_numCols = numCols;
 }
 
 /* 
@@ -187,7 +282,7 @@ OPERATOR OVERLOADING
 
 // compare operator
 template <class T>
-bool Matrix2<T>::operator== (const Matrix2<T>& rhs)
+bool Matrix<T>::operator== (const Matrix<T>& rhs) const
 {
     assert(m_numRows == rhs.numRows());
     assert(m_numCols == rhs.numCols());
@@ -202,12 +297,13 @@ bool Matrix2<T>::operator== (const Matrix2<T>& rhs)
             }
         }
     }
+    
     return true;
 }
 
 // assignment operator
 template <class T>
-Matrix2<T>& Matrix2<T>::operator= (const Matrix2<T>& rhs)
+Matrix<T>& Matrix<T>::operator= (const Matrix<T>& rhs)
 {
     //std::cout << "Assignment" << std::endl;
     
@@ -231,12 +327,13 @@ Matrix2<T>& Matrix2<T>::operator= (const Matrix2<T>& rhs)
             }
         }
     }
+    
     return *(this);
 }
 
 // += operator
 template <class T>
-void Matrix2<T>::operator+= (const Matrix2<T>& rhs)
+void Matrix<T>::operator+= (const Matrix<T>& rhs)
 {
     assert(m_numRows == rhs.numRows());
     assert(m_numCols == rhs.numCols());
@@ -252,7 +349,7 @@ void Matrix2<T>::operator+= (const Matrix2<T>& rhs)
 
 // -= operator
 template <class T>
-void Matrix2<T>::operator-= (const Matrix2<T>& rhs)
+void Matrix<T>::operator-= (const Matrix<T>& rhs)
 {
     assert(m_numRows == rhs.numRows());
     assert(m_numCols == rhs.numCols());
@@ -268,7 +365,7 @@ void Matrix2<T>::operator-= (const Matrix2<T>& rhs)
 
 // *= operator
 template <class T>
-void Matrix2<T>::operator*= (const T& rhs)
+void Matrix<T>::operator*= (const T& rhs)
 {
     assert(m_numRows == rhs.numRows());
     assert(m_numCols == rhs.numCols());
@@ -284,7 +381,7 @@ void Matrix2<T>::operator*= (const T& rhs)
 
 // /= operator
 template <class T>
-void Matrix2<T>::operator/= (const T& rhs)
+void Matrix<T>::operator/= (const T& rhs)
 {
     assert(m_numRows == rhs.numRows());
     assert(m_numCols == rhs.numCols());
@@ -301,14 +398,14 @@ void Matrix2<T>::operator/= (const T& rhs)
 // + operator
 // matrix + matrix
 template <class T>
-Matrix2<T> operator+ (const Matrix2<T>& lhs, const Matrix2<T>& rhs)
+Matrix<T> operator+ (const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
     assert(lhs.numRows() == rhs.numRows());
     assert(lhs.numCols() == rhs.numCols());
     
     int numRows = lhs.numRows();
     int numCols = lhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -323,11 +420,11 @@ Matrix2<T> operator+ (const Matrix2<T>& lhs, const Matrix2<T>& rhs)
 
 // scalar + matrix
 template <class T>
-Matrix2<T> operator+ (const T& lhs, const Matrix2<T>& rhs)
+Matrix<T> operator+ (const T& lhs, const Matrix<T>& rhs)
 {
     int numRows = rhs.numRows();
     int numCols = rhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -342,11 +439,11 @@ Matrix2<T> operator+ (const T& lhs, const Matrix2<T>& rhs)
 
 // matrix + scalar
 template <class T>
-Matrix2<T> operator+ (const Matrix2<T>& lhs, const T& rhs)
+Matrix<T> operator+ (const Matrix<T>& lhs, const T& rhs)
 {
     int numRows = lhs.numRows();
     int numCols = lhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -362,14 +459,14 @@ Matrix2<T> operator+ (const Matrix2<T>& lhs, const T& rhs)
 // - operator
 // matrix - matrix
 template <class T>
-Matrix2<T> operator- (const Matrix2<T>& lhs, const Matrix2<T>& rhs)
+Matrix<T> operator- (const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
     assert(lhs.numRows() == rhs.numRows());
     assert(lhs.numCols() == rhs.numCols());
     
     int numRows = lhs.numRows();
     int numCols = lhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -384,11 +481,11 @@ Matrix2<T> operator- (const Matrix2<T>& lhs, const Matrix2<T>& rhs)
 
 // scalar - matrix
 template <class T>
-Matrix2<T> operator- (const T& lhs, const Matrix2<T>& rhs)
+Matrix<T> operator- (const T& lhs, const Matrix<T>& rhs)
 {
     int numRows = rhs.numRows();
     int numCols = rhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -403,11 +500,11 @@ Matrix2<T> operator- (const T& lhs, const Matrix2<T>& rhs)
 
 // matrix - scalar
 template <class T>
-Matrix2<T> operator- (const Matrix2<T>& lhs, const T& rhs)
+Matrix<T> operator- (const Matrix<T>& lhs, const T& rhs)
 {
     int numRows = lhs.numRows();
     int numCols = lhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -423,13 +520,13 @@ Matrix2<T> operator- (const Matrix2<T>& lhs, const T& rhs)
 // * operator
 // matrix * matrix
 template <class T>
-Matrix2<T> operator* (const Matrix2<T>& lhs, const Matrix2<T>& rhs)
+Matrix<T> operator* (const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
     assert(lhs.numCols() == rhs.numRows());
     
     int numRows = lhs.numRows();
     int numCols = rhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -449,11 +546,11 @@ Matrix2<T> operator* (const Matrix2<T>& lhs, const Matrix2<T>& rhs)
 
 // scalar * matrix
 template <class T>
-Matrix2<T> operator* (const T& lhs, const Matrix2<T>& rhs)
+Matrix<T> operator* (const T& lhs, const Matrix<T>& rhs)
 {
     int numRows = rhs.numRows();
     int numCols = rhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -468,11 +565,11 @@ Matrix2<T> operator* (const T& lhs, const Matrix2<T>& rhs)
 
 // matrix * scalar
 template <class T>
-Matrix2<T> operator* (const Matrix2<T>& lhs, const T& rhs)
+Matrix<T> operator* (const Matrix<T>& lhs, const T& rhs)
 {
     int numRows = lhs.numRows();
     int numCols = lhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -487,11 +584,11 @@ Matrix2<T> operator* (const Matrix2<T>& lhs, const T& rhs)
 
 // matrix / scalar
 template <class T>
-Matrix2<T> operator/ (const Matrix2<T>& lhs, const T& rhs)
+Matrix<T> operator/ (const Matrix<T>& lhs, const T& rhs)
 {
     int numRows = lhs.numRows();
     int numCols = lhs.numCols();
-    Matrix2<T> result(numRows, numCols);
+    Matrix<T> result(numRows, numCols);
     
     for (int i = 0; i < numRows; ++i)
     {
@@ -506,11 +603,11 @@ Matrix2<T> operator/ (const Matrix2<T>& lhs, const T& rhs)
 
 /* 
 ======================================================================
-PRINT FUNCTIONS
+USEFUL FUNCTIONS
 */ 
 
 template <class T>
-void Matrix2<T>::print() const
+void Matrix<T>::print() const
 {
     for (int i = 0; i < m_numRows; ++i)
     {
@@ -524,12 +621,117 @@ void Matrix2<T>::print() const
 
 /* 
 ======================================================================
+MATRIX FUNCTIONS
+*/ 
+
+// resize matrix to new shape
+template <class T>
+void Matrix<T>::resize(int numRows, int numCols)
+{
+    assert(numRows * numCols == m_numRows * m_numCols);
+    
+    m_numRows = numRows;
+    m_numCols = numCols;
+}
+
+// Row/col operations
+
+template <class T>
+void Matrix<T>::addRow(int row, const Matrix<T>& matrix)
+{
+    assert((row >= 0) && (row < m_numRows));
+    assert(matrix.numRows() == 1);
+    assert(matrix.numCols() == m_numCols);
+    
+    setRow(row, getRow(row) + matrix);
+}
+
+template <class T>
+void Matrix<T>::addCol(int col, const Matrix<T>& matrix)
+{
+    assert((col >= 0) && (col < m_numCols));
+    assert(matrix.numCols() == 1);
+    assert(matrix.numRows() == m_numRows);
+    
+    setCol(col, getCol(col) + matrix);
+}
+
+template <class T>
+void Matrix<T>::subtractRow(int row, const Matrix<T>& matrix)
+{
+    assert((row >= 0) && (row < m_numRows));
+    assert(matrix.numRows() == 1);
+    assert(matrix.numCols() == m_numCols);
+    
+    setRow(row, getRow(row) - matrix);
+}
+
+template <class T>
+void Matrix<T>::subtractCol(int col, const Matrix<T>& matrix)
+{
+    assert((col >= 0) && (col < m_numCols));
+    assert(matrix.numCols() == 1);
+    assert(matrix.numRows() == m_numRows);
+    
+    setCol(col, getCol(col) - matrix);
+}
+
+template <class T>
+void Matrix<T>::scaleRow(int row, const T& coef)
+{
+    assert((row >= 0) && (row < m_numRows));
+    
+    setRow(row, getRow(row) * coef);
+}
+
+template <class T>
+void Matrix<T>::scaleCol(int col, const T& coef)
+{
+    assert((col >= 0) && (col < m_numCols));
+    
+    setCol(col, getCol(col) * coef);
+}
+
+// Row reduction operations
+
+template <class T>
+void Matrix<T>::replaceRow(int row0, int row1, const T& coef)
+{
+    addRow(row0, coef * getRow(row1));
+}
+
+template <class T>
+void Matrix<T>::swapRow(int row0, int row1)
+{
+    Matrix<T> temp = getRow(row0);
+    setRow(row0, getRow(row1));
+    setRow(row1, temp);
+}
+
+// return matrix in echelon form
+template <class T>
+Matrix<T> Matrix<T>::echelonForm()
+{
+    Matrix<T> result = *this;
+    
+    // iterate through each col and ensure leading row at top
+    for (int i = 0; i < m_numCols; ++i)
+    {
+
+    }
+    
+    return result;
+}
+
+
+/* 
+======================================================================
 PRIVATE FUNCTIONS
 */ 
 
 // returns index for row-ordered matrix
 template <class T>
-int Matrix2<T>::index(int row, int col) const
+int Matrix<T>::index(int row, int col) const
 {    
     return row*m_numCols + col;
 }
--- testing/linearAlgebra-testing/linearAlgebra.cpp
+++ testing/linearAlgebra-testing/linearAlgebra.cpp
@@ -6,15 +6,16 @@
 int main() 
 {
     double data1[] = {1.0, 2.0, 3.0, 4.0};
-    double data2[] = {2.0, 3.0, 1.0, 5.0, 3.0, 8.0};
+    double data2[] = {2.0, 3.0, 1.0, 5.0, 3.0, 8.0, 1.0, 2.0, 3.0};
     
-    Matrix2<double> mat1(2,2,data1);
-    Matrix2<double> mat2(2,3,data2);
+    Matrix<double> mat1(2,2,data1);
+    Matrix<double> mat2(3,3,data2);
     
-    Matrix2<double> mat3 = mat1;
+    mat2.print();
     
-    Matrix2<double> mat4;
+    std::cout<<"========="<<std::endl;
     
-    mat4 = mat2;
+    mat2.replaceRow(1, 0, -mat2.get(1,0)/mat2.get(0,0));
     
+    mat2.print();
 }
